77b4d19024755c8fd97c9b169ff8b088a4b213fb
AIZAWA-TOMO <116964452+TOMO-CHANN@users.noreply.github.com>
2023-06-30 13:50:55 +0530

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -9,10 +9,11 @@ if (!gg) {
 global.owner = gg.split(",");
 global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
 global.sessionId = process.env.SESSION_ID || "ok";
-global.prefa = process.env.PREFIX || "-";
+global.prefa = process.env.PREFIX || "#";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas Bot`;
-global.author = process.env.AUTHOR || "by: Team Atlas";
+global.packname = process.env.PACKNAME || `🎋 𝑯𝑬𝑹𝑬 𝒀𝑶𝑼 𝑮𝑶`
+global.author = process.env.AUTHOR || '𝞒𝞓𝞜𝞙 - 𝙎 || 𝚩𝚯𝚻 || 🎋'
+  ";
 global.port = process.env.PORT || "10000";
 global.openAiAPI = process.env.OPENAI_API || "Put your openai API key here";
 global.owner = gg.split(",");
